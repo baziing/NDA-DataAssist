99809a5866e035d314b54243f61abe9f17bbc9fa
baziing <sarumisarumi@hotmail.com>
2025-02-09 13:15:52 +0800

    【新增】手动报表
    输出路径

--- src/api/report_api.py
+++ src/api/report_api.py
@@ -3,12 +3,14 @@ from flask_cors import CORS  # 导入 CORS
 import os
 import sys
 import uuid
+import os
 
 # 将tools目录添加到Python路径
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'tools')))
 
-# 导入report_generator.py中的函数
+# 导入report_generator.py中的函数和config模块
 from report_generator import process_single_file
+from config import OUTPUT_DIR, DB_CONFIG
 
 app = Flask(__name__)
 CORS(app)  # 启用 CORS
@@ -33,10 +35,17 @@ def upload_file():
         
         # 调用report_generator.py中的函数处理文件
         try:
+            # 临时修改输出目录
+            original_output_dir = OUTPUT_DIR['report']
+            OUTPUT_DIR['report'] = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'output'))
             process_single_file(filepath)
-            return jsonify({'message': 'File uploaded and processed successfully'}), 200
+            
         except Exception as e:
             return jsonify({'error': str(e)}), 500
+        finally:
+            # 恢复输出目录
+            OUTPUT_DIR['report'] = original_output_dir
+        return jsonify({'message': 'File uploaded and processed successfully'}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
