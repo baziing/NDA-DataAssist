042a22d1777a0f77e1ff05ffa1ea1ebdc4001230
baziing <sarumisarumi@hotmail.com>
2025-02-17 12:10:31 +0800

    【修改】修改icon

--- src/router/index.js
+++ src/router/index.js
@@ -81,7 +81,7 @@ export const constantRoutes = [
         path: 'dashboard',
         component: () => import('@/views/dashboard/index'),
         name: 'Dashboard',
-        meta: { title: 'Dashboard', icon: 'dashboard', affix: true }
+        meta: { title: '系统介绍', icon: 'el-icon-s-opportunity', affix: true }
       }
     ]
   },
